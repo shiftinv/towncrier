7192401f4741425a1dfd76ff4fe21a32bcdbedc1
shiftinv <me@shiftinv.cc>
2022-08-11 01:26:12 +0200

    fix: re-export `load_config_from_file` for sphinxcontrib-towncrier

--- src/towncrier/_settings/__init__.py
+++ src/towncrier/_settings/__init__.py
@@ -6,10 +6,12 @@ from towncrier._settings import load
 load_config = load.load_config
 ConfigError = load.ConfigError
 load_config_from_options = load.load_config_from_options
+load_config_from_file = load.load_config_from_file
 
 
 __all__ = [
     "load_config",
     "ConfigError",
     "load_config_from_options",
+    "load_config_from_file",
 ]
